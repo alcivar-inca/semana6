2b893f6594970f74e5e82a2ffaba8b76e97dd991
alcivar-inca <eddy_epn2008@hotmail.com>
2016-05-26 08:21:14 -0500

    Se añadio funcion promedio de numeros ingreadso

--- area_circulo.py
+++ area_circulo.py
@@ -6,4 +6,19 @@ def calcular():
 	area=round(math.pi*(radio*radio), 2)
 	print("El area del circulo es: " + str(area))
 
+# PROMEDIO DE NUMEROS INGRESADOS
+def ingresoNumeros():
+	num =1
+	cont=0
+	resp =0
+	while num>0:
+		num=int(input("Ingrese un numero: "))
+		if num!=0:
+			resp= resp + num
+			cont=cont+1
+	
+	print("El promedio es: " + str(resp/cont))
+
+
 calcular()
+ingresoNumeros()
